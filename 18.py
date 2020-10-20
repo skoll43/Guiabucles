93bda63e9c29766fc19316b6a9c75d3ea680baf1
skoll43 <54006305+skoll43@users.noreply.github.com>
2020-10-20 18:10:20 -0300

    Update 18.py

--- 18.py
+++ 18.py
@@ -2,7 +2,7 @@
 #aque la suma de los dígitos pares es igual a la suma de los dígitos impares.
 cuentapares = 0
 cuentaimpares = 0
-for i in range(10):
+for i in range(1,10):
     if i % 2 == 0:
         for j in range(10):
             if j % 2 == 0:
